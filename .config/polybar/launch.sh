6280ac2b895c0964ec97d1a1c1124a1c73f13d52
Ross Edwards <redwards64@hotmail.com>
2022-06-02 03:33:21 +0100

    added polybar config

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# terminate already running bar instances
+killall -q polybar
+# if all bars have ipc enabled, use
+# polybar-msg cmd quit
+
+# launch polybar using default config location ~/.config/polybar/config.ini
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+
+echo "Polybar launched..."
