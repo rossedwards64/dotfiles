e4149c3f1d0c42a39a455f11890a6acd02656476
Ross Edwards <redwards64@hotmail.com>
2022-09-28 18:45:38 +0100

    improved muting and power menu

--- .config/hypr/scripts/check_mute.sh
+++ .config/hypr/scripts/check_mute.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ $1 == "getspeaker" ]]; then
-    status=$(amixer sget Master | tail -1 | cut -c 37-38)
+    status=$(amixer sget Master | tail -1 | cut -c 38-39)
     if [[ $status == "of" ]]; then
         notify-send "Muted Speaker"
     elif [[ $status == "on" ]]; then
