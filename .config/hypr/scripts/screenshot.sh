719d12896a90f6794821d4f60f19d0683af2440c
Ross Edwards <redwards64@hotmail.com>
2022-09-28 18:45:38 +0100

    added scripts to hyprland

---
+++ .config/hypr/scripts/screenshot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ $# -eq 0 ]]; then
+    grim -g "$(slurp)" - | wl-copy && notify-send "Screenshot taken" "Copied to clipboard"
+else
+    screenshots_dir="$1"
+    grim -g "$(slurp)" - > "$screenshots_dir/$(date +%Y%m%d%H%M%S).png" && notify-send "Screenshot taken" "Saved to $screenshots_dir"
+fi
