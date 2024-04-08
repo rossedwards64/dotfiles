e62521e15082e53862279bb8d590308f3d96e3fa
Ross Edwards <redwards64@hotmail.com>
2024-04-08 13:36:34 +0100

    remove unnecessary services module

---
+++ modules/nixos/services/default.nix
@@ -0,0 +1,12 @@
+{ lib, config, pkgs, ... }:
+with lib;
+let cfg = config.modules.services;
+in {
+  options.modules.services = { enable = mkEnableOption "services"; };
+
+  config = mkIf cfg.enable {
+    services = {
+
+    };
+  };
+}
