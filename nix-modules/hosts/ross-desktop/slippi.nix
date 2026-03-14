673ab887b2035ce2d5756195196f4380ccf58f28
Ross Edwards <redwards64@hotmail.com>
2026-03-14 14:28:40 +0000

    slippi - add config

--- nix-modules/hosts/ross-desktop/slippi.nix
+++ nix-modules/hosts/ross-desktop/slippi.nix
@@ -1,6 +1,24 @@
 { inputs, config, ... }:
+let
+  inherit (config.flake.meta.user) username;
+in
 {
-  configurations.home."${config.flake.meta.user.username}@ross-desktop".module = {
-    imports = [ inputs.slippi-nix.homeManagerModules.default ];
+  configurations = {
+    nixos.ross-desktop.module = {
+      imports = [ inputs.slippi-nix.nixosModules.default ];
+    };
+
+    home."${username}@ross-desktop".module = {
+      imports = [ inputs.slippi-nix.homeManagerModules.default ];
+
+      slippi-launcher = {
+        isoPath = "/SSD/Games/GameCube and Wii/Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso";
+        launchMeleeOnPlay = true;
+        enableJukebox = true;
+        rootSlpPath = "/home/${username}/Documents/Slippi/Matches";
+        spectateSlpPath = "/home/${username}/Documents/Slippi/Spectate";
+        useMonthlySubfolders = true;
+      };
+    };
   };
 }
