ff11ff03ce2144b55207f38da2587e34181b5cd8
Ross Edwards <redwards64@hotmail.com>
2026-03-14 14:28:16 +0000

    niri - prevent cursor from scrolling the workspace

--- nix-modules/window-manager/niri/input.nix
+++ nix-modules/window-manager/niri/input.nix
@@ -1,6 +1,9 @@
 {
   flake.modules.homeManager.base.programs.niri.settings.input = {
     workspace-auto-back-and-forth = true;
-    focus-follows-mouse.enable = true;
+    focus-follows-mouse = {
+      enable = true;
+      max-scroll-amount = "0%";
+    };
   };
 }
